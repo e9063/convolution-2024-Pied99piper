0c55f7ce89052961f5b554d67aad0c7bffbb94cb
Pied99piper <worapol.wa@ku.th>
2024-10-30 00:41:12 +0700

    change code

--- conv_parallel.c
+++ conv_parallel.c
@@ -5,7 +5,6 @@
 
 int main(){
     int NA, NF;
-    double start, end, parallel_time;
     scanf("%d %d", &NA, &NF);
     int *A = malloc(sizeof(int) * NA);
     int *F = malloc(sizeof(int) * NF);
@@ -17,20 +16,22 @@ int main(){
         scanf("%d", &F[i]);
     }
 
+    int* FF = (int*)malloc(NF * sizeof(int));
+    for (int j = 0 ; j < NF ; ++j) {
+        FF[j] = F[NF - 1 - j];
+    }
+
     int* R = (int*)malloc((NA - NF + 1) * sizeof(int));
-    start = omp_get_wtime();
     omp_set_num_threads(4);
     #pragma omp parallel for schedule(static)
-    for (int i = 0; i < NA - NF + 1; i++) {
-        R[i] = 0;
-        for (int j = 0; j < NF; j++) {
-            R[i] += A[i + j] * F[NF - 1 - j];
+    for (int i = 0 ; i < NA - NF + 1 ; i++) {
+        int t = 0;
+        for (int j = 0 ; j < NF ; j++) {
+            t += (A[i + j] * FF[j]);
         }
+        R[i] = t;
     }
-    end = omp_get_wtime();
-    parallel_time = end - start;
 
-    // printf("parallel time : %f\n", parallel_time);
     for (int i = 0 ; i < NA - NF + 1 ; i++) {
         printf("%d\n",R[i]);
     }
